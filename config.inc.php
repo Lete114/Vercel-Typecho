1714ea81efd3d4b2e150af22a2d6ad2aaf642481
naya2016 <63491927+naya2016@users.noreply.github.com>
2022-11-20 15:41:03 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -36,12 +36,12 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => '数据库地址',
-  'user' => '数据库用户',
-  'password' => '数据库密码',
+  'host' => 'queenie.db.elephantsql.com',
+  'user' => 'bhvicgdh',
+  'password' => 'byQ8c31R4tswc3pA6jwqSi8mI3JK7Xyl',
   'charset' => 'utf8mb4',
   'port' => '3306',
-  'database' => '数据库名',
+  'database' => 'bhvicgdh',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
